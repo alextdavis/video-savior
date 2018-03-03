5776c5ef640003df6bbc129a43e8a6e991cbb115
Alex T. Davis <git@alextdavis.me>
2018-03-02 18:51:49 -0600

    Initial Commit

---
+++ Souviens-Mac/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Souviens-Mac
+//
+//  Created by Alex Davis on 3/2/18.
+//  Copyright © 2018 Alex T. Davis. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Souviens-Mac/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Souviens-Mac
+//
+//  Created by Alex Davis on 3/2/18.
+//  Copyright © 2018 Alex T. Davis. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
